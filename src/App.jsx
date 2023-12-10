ec1c2a8064094f2dc5359d78354ed5b2bdb222ec
Daiki <hadaiki2009@gmail.com>
2023-12-10 14:58:18 +1100

    fetch data from external api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 
-
-
-const getAsyncStories = () =>
-  new Promise((resolve, reject) => setTimeout(reject, 2000));
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -49,13 +46,18 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories().then(result => {
-      dispatchStories({
-        type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
-      });
-    })
-    .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+    fetch(`${API_ENDPOINT}react`) // B
+      .then((response) => response.json()) // C
+      .then((result) => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits, // D
+        });
+      })
+      .catch(() =>
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+      );
+
   }, []);
 
   const handleRemoveStory = (item) => {
