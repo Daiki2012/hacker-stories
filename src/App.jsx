7996954fb29c7c0b975688ab322faff1d4b59b39
Daiki <hadaiki2009@gmail.com>
2023-12-23 16:31:11 +1100

    Add React Form

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,27 @@ const storiesReducer = (state, action) => {
   }
 };
 
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState('search','');
 
@@ -47,8 +68,9 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
   };
 
   const [stories, dispatchStories] = React.useReducer(
@@ -87,21 +109,11 @@ const App = () => {
   return (
   <div>
     <h1>My Hacker Stories</h1>
-    <InputWithLabel
-        id="search"
-        value={searchTerm}
-        onInputChange={handleSearchInput}
-      >
-        Search:
-      </InputWithLabel>
-
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+    <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
     <hr />
     {stories.isError && <p>Something went wrong ...</p>}
 
