35f1f314d8e4295d95b4e423f867b241d852d7e6
Daiki <hadaiki2009@gmail.com>
2023-12-29 18:41:42 +1100

    update react unit test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -33,6 +33,41 @@ const storyTwo = {
 
 const stories = [storyOne, storyTwo];
 
+describe('SearchForm', () => {
+  const searchFormProps = {
+    searchTerm: 'React',
+    onSearchInput: vi.fn(),
+    onSearchSubmit: vi.fn(),
+  };
+
+  it('renders the input field with its value', () => {
+    render(<SearchForm {...searchFormProps} />);
+
+    screen.debug();
+    expect(screen.getByDisplayValue('React')).toBeInTheDocument();
+  });
+
+  it('renders the correct label', () => {
+    render(<SearchForm {...searchFormProps} />);
+
+    expect(screen.getByLabelText(/Search/)).toBeInTheDocument();
+  });
+
+  it('calls onSearchInput on input field change', () => {
+    render(<SearchForm {...searchFormProps} />);
+    fireEvent.change(screen.getByDisplayValue('React'), {
+      target: { value: 'Redux' },
+    });
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls onSearchSubmit on button submit click', () => {
+    render(<SearchForm {...searchFormProps} />);
+    fireEvent.submit(screen.getByRole('button'));
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+  });
+});
+
 describe('storiesReducer', () => {
   it('removes a story from all stories', () => {
     const action = { type: 'REMOVE_STORY', payload: storyOne };
