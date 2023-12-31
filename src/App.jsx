481ed39502c5375a583c093ec73541778304aa33
Daiki <hadaiki2009@gmail.com>
2023-12-31 15:46:25 +1100

    split each component into multiple jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import axios from 'axios';
+import { SearchForm } from './SearchForm';
+import { List } from './List';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -36,29 +38,8 @@ const storiesReducer = (state, action) => {
   }
 };
 
-const SearchForm = ({
-  searchTerm,
-  onSearchInput,
-  onSearchSubmit,
-}) => (
-  <form onSubmit={onSearchSubmit}>
-    <InputWithLabel
-      id="search"
-      value={searchTerm}
-      isFocused
-      onInputChange={onSearchInput}
-    >
-      <strong>Search:</strong>
-    </InputWithLabel>
-
-    <button type="submit" disabled={!searchTerm}>
-      Submit
-    </button>
-  </form>
-);
-
 const App = () => {
-  const [searchTerm, setSearchTerm] = useStorageState('search','');
+  const [searchTerm, setSearchTerm] = useStorageState('search','React');
 
   const [url, setUrl] = React.useState(
     `${API_ENDPOINT}${searchTerm}`
@@ -125,54 +106,6 @@ const App = () => {
   </div>
   );
 }
-
-const List = ({ list, onRemoveItem }) => (
-  <ul>
-    {list.map((item) => (
-      <Item
-        key={item.objectID}
-        item={item}
-        onRemoveItem={onRemoveItem}
-      />
-    ))}
-  </ul>
-)
-
-const Item = ({ item, onRemoveItem }) => (
-  <li>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={onRemoveItem.bind(null, item)}>
-        Dismiss
-      </button>
-    </span>
-  </li>
-);
-
-const InputWithLabel = ({id, value, type = 'text', onInputChange, isFocused, children}) => {
-  // A
-  const inputRef = React.useRef();
-  // C
-  React.useEffect(() => {
-    if (isFocused && inputRef.current) {
-      // D
-      inputRef.current.focus();
-    }
-  }, [isFocused]);
-  return (
-    <>
-      <label htmlFor={id}>{children}</label>
-      &nbsp;
-       {/* B */}
-      <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} />
-    </>
-  );
-};
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
